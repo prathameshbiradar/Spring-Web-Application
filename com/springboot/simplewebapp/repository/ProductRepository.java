688f8eea21cc25a51e1392e7a92f5bf5cc55edc1
prathmeshbiradar <psbiradar948@gmail.com>
2025-02-12 12:19:09 +0530

    Commited CRUD operation using RESTAPI Protocols

--- com/springboot/simplewebapp/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.simplewebapp.repository;
-
-import com.springboot.simplewebapp.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
